114aff674f97af4f84b4dfae57f8a33b97bba1bc
eeeee1100 <jassonvrhess@gmail.com>
2025-11-13 12:54:07 -0600

    - Cambios realizados: Se añadieron las clases GUIGameOver, GUIWin, Puntaje y Cronometro

---
+++ src/main/java/com/mycompany/parchisc5d790/modelo/Puntaje.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.parchisc5d790.modelo;
+
+/**
+ *
+ * @author UCR
+ */
+public class Puntaje {
+    
+}
