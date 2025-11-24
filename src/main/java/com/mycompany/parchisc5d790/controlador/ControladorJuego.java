50a6976bc49798c26ecc3f2ad8a409095b7027b0
eeeee1100 <jassonvrhess@gmail.com>
2025-11-24 08:45:42 -0600

    - Cambios realizados: Clases Tablero y AreaJuego arrgladas. Añadidos métodos en clase Cronometro. Incompleto.

--- src/main/java/com/mycompany/parchisc5d790/controlador/ControladorJuego.java
+++ src/main/java/com/mycompany/parchisc5d790/controlador/ControladorJuego.java
@@ -5,6 +5,8 @@
 package com.mycompany.parchisc5d790.controlador;
 
 import com.mycompany.parchisc5d790.modelo.AreaJuego;
+import com.mycompany.parchisc5d790.modelo.Cronometro;
+import com.mycompany.parchisc5d790.modelo.Dado;
 import com.mycompany.parchisc5d790.modelo.Ficha;
 import com.mycompany.parchisc5d790.modelo.Posicion;
 import com.mycompany.parchisc5d790.vista.GUIJuego;
@@ -18,6 +20,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.ImageIcon;
+import javax.swing.Timer;
 
 /**
  *
@@ -30,61 +33,119 @@ public class ControladorJuego implements ActionListener, MouseListener {
     private GUIPrincipal guiPrincipal;
     private Ficha ficha;
     private AreaJuego areaJuego;
-    private Posicion posicion;
+    private Dado dado;
     
-    public ControladorJuego(GUIPrincipal guiPrincipal, String colorJugador1, String nombreJugador1, String nombreJugador2){
-        this.guiPrincipal=guiPrincipal;
-        panelJuego=guiJuego.getPanelJuego();
-        panelControl=guiJuego.getPanelControl();
+    private int resultadoDado = 0;
+    private boolean turnoTerminado = true;
+
+    public ControladorJuego(GUIPrincipal guiPrincipal, String colorJugador1, String nombreJugador1,  String nombreJugador2) {
+        this.guiPrincipal = guiPrincipal;
         guiJuego = new GUIJuego(this);
+        panelJuego = guiJuego.getPanelJuego();
+        areaJuego = new AreaJuego(colorJugador1,nombreJugador1,nombreJugador2);
+        panelControl = guiJuego.getPanelControl();
+        panelControl.setJlNombreJugador1(nombreJugador1);
+        panelControl.setJlNombreJugador2(nombreJugador2);
+        dado = new Dado();
         guiJuego.setVisible(true);
-        areaJuego = new AreaJuego(colorJugador1,nombreJugador1, nombreJugador2);
-        ficha = new Ficha(new Posicion(453,468),new ImageIcon("./src/main/resources/img/piecered.png"),"Amarillo");
+        
+        //Aficha = new Ficha(new Posicion(100, 100), new ImageIcon("./src/main/resources/img/pieceblue.png"), "Amarillo");
+        //453,468
     }
 
-    public void dibujar(Component c, Graphics g){
-        ficha.dibujar(c,g);
+    public void dibujar(Component c, Graphics g) {
+
+        //ficha.dibujar(c, g);
+        areaJuego.dibujar(c, g);
+
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        switch(e.getActionCommand()){
+
+        switch (e.getActionCommand()) {
+
             case "Dado":
-                System.out.println("Presionó el dado");
+                if(turnoTerminado){
+                    resultadoDado = dado.tiro();
+                    System.out.println("Resultado de dado:" + resultadoDado);
+                    String rutaImagen = "/img/dado" + resultadoDado + ".1.png";
+                    ImageIcon iconoDado = new ImageIcon(getClass().getResource(rutaImagen));
+                    javax.swing.JButton btn = panelControl.getBtnDado();
+                    btn.setIcon(iconoDado);
+                    btn.setText("");
+                    panelJuego.repaint();
+                    
+                    if(resultadoDado == 5){
+                        turnoTerminado = false;
+                        System.out.println("Jugador 1: !Saca una ficha de la casa!");
+                        
+                    } else {
+                    
+                        turnoTerminado = true;
+                    
+                    }
+                
+                } else {
+                
+                    System.out.println("Debes mover una ficha antes de volver a tirar el dado.");
+                
+                }
                 break;
-            case "JButton1":
-                 guiJuego.dispose();
-                 guiPrincipal.setVisible(true);
+
+            case "AtrasJuego":
+                guiJuego.dispose();
+                guiPrincipal.setVisible(true);
                 break;
+
         }
+
     }
-    
-    int indiceCelda = areaJuego.getIndexFicha(e.getX(), e.getY());
-    if indice
 
     @Override
     public void mouseClicked(MouseEvent e) {
-
+        System.out.println("X " + e.getX() + " Y " + e.getY());
+        if(resultadoDado == 5 && !turnoTerminado){
         
+          if(areaJuego.isContains(e.getX(), e.getY())){
+          
+              int indexFicha = areaJuego.getIndexFicha(e.getX(), e.getY());
+              
+              if (indexFicha != -1){
+              
+                  System.out.println("Moviendo ficha en indice de casa: " + indexFicha);
+                  
+                  areaJuego.sacarFicha(indexFicha);
+                  
+                  turnoTerminado = true;
+                  resultadoDado = 0;
+              
+              }
+          
+          }
+        
+        }
+        panelJuego.repaint();
     }
-    
+
     @Override
     public void mousePressed(MouseEvent e) {
-        
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-       
+
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-       
+
     }
+
 }
